00a4aedf6db414701358b62e2ed955983a4f6151
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-05-31 23:46:19 +0300

    release 2.0.0 (#2)
    * 1 Managed loading random img from Api request.
    
    * 2 Created <buttom> for loading image.
    
    * 3 Extend properties DOM element via Object.defineProperties()
    
    Using DOM element Object.defineProperties() added appiRequest in order to use it as one properties inside event.target.
    
    * 4 Modified function loadImg, created class CardDog
    
    In function loadImg using e.target.nexElementSibling in order to call modification of current unique elemnt.
    
    * 5 Set uped the loading of web pages using only class CardDog
    
    * 6 Created class CardDogHtmlElement extends from HTML element
    
    CardDogHtmlElement include nesting of all tags that represent object Card-Dogs.

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
 document.addEventListener('DOMContentLoaded', (e)=>{
     console.log('Start');
+ 
+    let api = 'https://dog.ceo/api/breeds/image/random';
 
-    createCard(document.getElementById('card_dog'));
-    let apiRequest = 'https://dog.ceo/api/breeds/image/random';
+    const cardDog1 = new CardDog('dog1', api);
+    document.querySelector('body').appendChild(cardDog1.divIDCardDog);
 
-    document.querySelector('button').addEventListener('click', (e) => {
-        loadImg(apiRequest); 
-    });   
+    const cardDogHtml2 = new CardDogHtmlElemnt('dog2', api);
+    document.querySelector('body').appendChild(cardDogHtml2);
+
 })
-
-const loadImg = (apiRequest) => {
-    fetch(apiRequest).
-    then(res => {
-        if(res.status === 200){
-            return res.json();
-        }
-        else{
-            throw new Error();
-        }
-    }).
-    then(apiObj => {
-
-        if(apiObj.status !== 'success'){
-            throw 'Error!\nImg did not load!'
-        }
-        document.querySelector('div.img_dog').style.backgroundImage = `url(${apiObj.message})`;
-    }).
-    catch(error => {
-        console.log(error);
-        document.querySelector('div.img_dog').style.backgroundImage = "url('./img/error404.jpg')";
-    })
-    .finally(() => {
-        console.log('End');
-    });
-}
-
-const createCard = (element) => {
-    let divDog = document.createElement('div');
-    divDog.className = 'img_dog';
-    divDog.style.backgroundImage = "url('./img/load.png')"
-
-    element.appendChild(divDog);
-}
