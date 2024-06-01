eda83d5216983d51b993ba1c7d998e1656a1823b
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-01 17:30:20 +0300

    2 Connect event 'click' of btn for 'Breed' with function createDropDownList()

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ document.addEventListener('DOMContentLoaded', async (e)=>{
     console.log('Start');
  
     const api = 'https://dog.ceo/api/breeds/list';
+    // const nameBreeds = 's_breeds';
+    // const sizeList = 5;
 
-    // let selectTag = document.getElementsByName('s_sub_breeds')[0];
-    // selectTag.add(new Option('one', '1'));
-    // selectTag.options[selectTag.options.length] = new Option('two', '2');
-    // selectTag['2'] = new Option('three', '3');
+    const btnBreed = document.getElementById('btn_breed');
+    Object.defineProperties(btnBreed, {apiRequest: {value: api, writable: false}});   
+    Object.defineProperties(btnBreed, {nameBreeds: {value: 's_breeds', writable: false}});   
+    Object.defineProperties(btnBreed, {sizeList: {value: 5, writable: false}});   
 
-    // let arr = ['one', 'two', 'three'];
-    // arr.forEach((element, index) => {
-    //     console.log(element, index);
-    // })
+    btnBreed.addEventListener('click', createDropDownBreeds);
 
+})
+
+
+const createDropDownBreeds = async (e) => {
     try{
-        const breeds = await getBreeds(api);
-        setSelection(document.getElementsByName('s_breeds')[0], breeds);
+        const breeds = await getBreeds(e.target.apiRequest);
+        e.target.nextElementSibling.appendChild(createSelectTag(e.target.nameBreeds, e.target.sizeList));
+
+        setSelection(document.getElementsByName(e.target.nameBreeds)[0], breeds);
+
+        document.getElementsByName('s_breeds')[0].addEventListener('change', (e)=> {
+        const selectedOption = e.target.options[e.target.selectedIndex];
+        console.log(selectedOption.text);
+    })
     }
     catch(ex){
         console.log(ex)
-    }   
-})
+    }
+}
+
+const createSelectTag = (name, size) => {
+    const selectTag = document.createElement('select');
+    selectTag.name = name;
+    selectTag.size = size;
+
+    return selectTag;
+}
 
 const setSelection = (s_element, array) => {
     array.forEach((name, index) =>{
         s_element.add(new Option(name, index));
     });
-    console.log(s_element);
 }
 
 const getBreeds = async(api) => {
